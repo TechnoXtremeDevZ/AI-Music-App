2ad58eb718b139a8fe95fc093536fd4c7ef2d16e
Shubhay S. Shah <69692903+TechnoXtremeDevZ@users.noreply.github.com>
2022-04-28 17:52:20 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var song_lav = "";
 var song_atc = "";
+var leftWristX = 0;
+var rightWristX = 0;
+var leftWristY = 0;
+var rightWristY = 0;
 
 function preload() {
     song_lav = loadSound("I'm So Tired (ft. Justin Beiber).mp3")
@@ -12,8 +16,26 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
 }
 
 function draw() {
     image(video, 0, 0, 400, 300);
+}
+
+function modelLoaded() {
+    console.log("Model is Loaded.");
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+    }
 }
